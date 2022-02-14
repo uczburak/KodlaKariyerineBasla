22c747b427e0ea7fabc5b2ebacac1d673b966795
Burak <uczbrk@gmail.com>
2022-02-14 19:13:10 +0300

    kosul bloklari islendi

---
+++ if-else/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time >= 6 && time < 11)
+                Console.WriteLine("Günaydın!");
+            else if(time <= 18)
+                Console.WriteLine("İyi Günler..");
+            else
+                Console.WriteLine("İyi geceler...");
+
+            string sonuc = time >= 6 && time < 12 ? "Good Morning!" : time <= 17 ? "Good Afternoon!" : "Good Night!";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
