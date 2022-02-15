421b4e7e872baa7a2cd89426774333e159c84643
Burak <uczbrk@gmail.com>
2022-02-15 19:34:45 +0300

    Array methodları incelendi

---
+++ arrays/arrays2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace arrays2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //sort
+            int[] sayiDizisi = {23,12,4,86,72,3,11,17};
+
+            Console.WriteLine("***** Sırasız Dizi *****");
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            Console.WriteLine("***** Sıralı Dizi *****");
+            Array.Sort(sayiDizisi);
+
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            //Clear
+            Console.WriteLine("***** Array Clear *****");
+            //Sayı dizisi elemanlarını kullanarak 2.indexten itibaren 2 tane elemanı sıfırlar
+            Array.Clear(sayiDizisi,2,2);
+
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            //Reverse
+            Console.WriteLine("***** Reverse Array *****");
+            Array.Reverse(sayiDizisi);
+
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            //IndexOf
+            Console.WriteLine("***** Array IndexOf *****");
+            Console.WriteLine(Array.IndexOf(sayiDizisi,23));
+
+            //Resize
+            Console.WriteLine("***** Array Resize *****");
+            Array.Resize<int>(ref sayiDizisi, 9);
+            sayiDizisi[8] = 99;
+
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+        }
+    }
+}
